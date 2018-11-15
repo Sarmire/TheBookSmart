ad5fa4c94f2bccf64a7b4d9c80851d714872fbec
Nizar127 <ahmadfakhrul127@gmail.com>
2018-11-16 01:30:28 +0800

    changes made on available biz

---
+++ app/src/main/java/com/thebooksmart/booksmart/Confirm_book.java
@@ -0,0 +1,17 @@
+package com.thebooksmart.booksmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Confirm_book extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_book);
+
+
+
+
+    }
+}
