f8a40a7470ccd8bf186656bb06764fd2927b63ac
Nizar127 <ahmadfakhrul127@gmail.com>
2018-11-18 13:19:35 +0800

    changes made on available biz

---
+++ app/src/main/java/com/thebooksmart/booksmart/ListBusinessActivity.java
@@ -0,0 +1,13 @@
+package com.thebooksmart.booksmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_business);
+    }
+}
