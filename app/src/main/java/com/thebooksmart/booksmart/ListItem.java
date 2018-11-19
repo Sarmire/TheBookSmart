64a04776a77dec04ff62b8d6d9233e2798169155
Nizar127 <ahmadfakhrul127@gmail.com>
2018-11-19 20:57:44 +0800

    changes made on available biz add new files listItem.xml and listAdapter for listing data and new updates on Mainactivity this time still work on RecyclerView

--- app/src/main/java/com/thebooksmart/booksmart/ListItem.java
+++ app/src/main/java/com/thebooksmart/booksmart/ListItem.java
@@ -6,12 +6,14 @@ public class ListItem {
     private String availBiz;
     private String availBizDetail;
     private String viewPackage;
+    private String imageURL;
 
-    public ListItem(String bizImg, String availBiz, String availBizDetail, String viewPackage) {
+    public ListItem(String bizImg, String availBiz, String availBizDetail, String viewPackage,String imageURL) {
         this.bizImg = bizImg;
         this.availBiz = availBiz;
         this.availBizDetail = availBizDetail;
         this.viewPackage = viewPackage;
+        this.imageURL = imageURL;
     }
 
     public String getBizImg() {
@@ -36,6 +38,10 @@ public class ListItem {
 
     public void setViewPackage(String viewPackage) {
         this.viewPackage = viewPackage;
+    }
+
+    public String getImageURL() {
+        return imageURL;
     }
 }
 
--- app/src/main/java/com/thebooksmart/booksmart/MainActivity.java
+++ app/src/main/java/com/thebooksmart/booksmart/MainActivity.java
@@ -1,6 +1,9 @@
 package com.thebooksmart.booksmart;
 
 import android.app.AlarmManager;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +17,17 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -27,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         //private FragmentPagerAdapter mSection;
         //private ViewPager mViewPager;
 
+    private static final String URL_DATA = "https://simplifiedcoding.net/demos/marvel/";
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
@@ -40,16 +55,66 @@ public class MainActivity extends AppCompatActivity {
             recyclerView = (RecyclerView) findViewById(R.id.listBiz);
             recyclerView.setHasFixedSize(true);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            listItems = new ArrayList<>();
+
+            loadRecyclerViewData();
+        }
+
+        private void loadRecyclerViewData(){
+
+            //this one is deprecated..i don't want to use toast either
+            //progress bar is seemingly unknown on how to use
+           /* ProgressDialog progressDialog  = new ProgressDialog(this);
+            progressDialog.setMessage("Loading Business...");
+            progressDialog.show();*/
+
+            //this is use volley
+           // StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                    //URL_DATA,
+                    //new Response.Listener<String>() {
+                       // @Override
+                        //public void onResponse(String s) {
+                        /*try{
+                            JSONObject jsonObject = new JSONObject(s);
+                            JSONArray array = jsonObject.getJSONArray("heroes");
+
+                            for(int i = 0; i<array.length();i++){
+                                JSONObject o = array.getJSONObject(i);
+                                ListItem item = new ListItem(
+                                        o.getString("name");
+                                        o.getString("about"),
+                                        o.getString("image");
+                                );
+                                listItems.add()
+                            }
+                        }
+                        catch (JSONArray e){
+                            e.printStackTrace();
+                        }
+                        }*/
+                   /* },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }*/
+//}
+
 
             //this one is inaccurate..maybe logic error
-            listItems = new ArrayList<>();
+           /* listItems = new ArrayList<>();
 
                 for(int i=0;i<10;i++){
                     ListItem listItem = new ListItem(
-                            "" + i+1,
-                            "" + i+1,
-                            "" + i+1,
-                            "" + i+1
+                            "bizimg" + (i+1),
+                            "availbiz" + (i+1),
+                            "availBizDetail" + (i+1),
+                            "viewPackage" + (i+1)
                     );
 
                     listItems.add(listItem);
@@ -59,11 +124,12 @@ public class MainActivity extends AppCompatActivity {
 
                 recyclerView.setAdapter(adapter);
             }
+*/
 
 
            // b1 = (Button)findViewById(R.id.button);
             //b1.setOnClickListener(new View.OnClickListener() {
-                @Override
+                //@Override
                 //public void onClick(View v) {
                 //    addNotification();
                 //}
@@ -101,9 +167,9 @@ public class MainActivity extends AppCompatActivity {
             //THIS IS HAVE TO BE FIXED ON ACTIVITY_MAIN.XML ON BOTTOM NAVIGATIONVIEW
 
 
-        }
+        //}
 
-        private void addNotification() {
+       /* private void addNotification(){
             NotificationCompat.Builder builder =
                     new NotificationCompat.Builder(this)
                             .setSmallIcon(R.drawable.ic_stat_name)
@@ -118,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
             // Add as notification
             NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             manager.notify(0, builder.build());
-        }
+        }*/
 
         //add alarm service when check in is coming
        // AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
@@ -127,5 +193,4 @@ public class MainActivity extends AppCompatActivity {
        // Intent intent = new Intent("")
        // PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, Intent(""), PendingIntent.FLAG_UPDATE_CURRENT);
         //alarmManager.setExact(AlarmManager.RTC_MAKEUP, calendar.getInTimeInMillis(),);
-    }
-
+    //}}
