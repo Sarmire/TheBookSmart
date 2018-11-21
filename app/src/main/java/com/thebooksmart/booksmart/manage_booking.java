9b5a71123d03a43d5d2c2d9d0820d9f1d079a7a9
Nizar127 <ahmadfakhrul127@gmail.com>
2018-11-21 15:23:11 +0800

    changes on new commit manage_booking manage_business

---
+++ app/src/main/java/com/thebooksmart/booksmart/manage_booking.java
@@ -0,0 +1,13 @@
+package com.thebooksmart.booksmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class manage_booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_booking);
+    }
+}
