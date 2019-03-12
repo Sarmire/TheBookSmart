cba4fb03c18fdf672149c3f09c0b77e5f66c7e4b
Sarmire <seusoff96@gmail.com>
2019-03-13 00:51:51 +0800

    test je ma

--- app/src/main/java/com/thebooksmart/booksmart/bizAvailList.java
+++ app/src/main/java/com/thebooksmart/booksmart/bizAvailList.java
@@ -1,4 +1,18 @@
 package com.thebooksmart.booksmart;
 
 public class bizAvailList {
+
+    private String bizname;
+
+    public bizAvailList(String bizname) {
+        this.bizname = bizname;
+    }
+
+    public String getBizname() {
+        return bizname;
+    }
+
+    public void setBizname(String bizname) {
+        this.bizname = bizname;
+    }
 }
